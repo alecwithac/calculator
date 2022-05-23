130d810a1a64c0385b75512ccbe1ab14286becb1
Alec <alecbolla@gmail.com>
2022-05-23 13:03:47 -0700

    added data numbers to buttons in html. Have foundation of javascript set. Calculator functioning properly. Will add functions to show operation symbols to calculator.

--- script.js
+++ script.js
@@ -1,3 +1,81 @@
+//constructor class for calculator
+
+class Calculator {
+  constructor(previousOperandTextElement,currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currentOperandTextElement
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined 
+  }
+
+  delete() {
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
+  }
+
+  //function to add number to screen
+  appendNumber(number) {
+    if (number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+  //function that uitilizes the operations a user chooses
+  chooseOperation(operation) {
+    if (this.currentOperand === '') return
+    if (this.previousOperand !== '') {
+      this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+  }
+  //calculates what user inputs into calculator
+  compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+      case '+':
+        computation = prev + current 
+        break
+      case '-':
+        computation = prev - current 
+        break
+      case '*':
+        computation = prev * current 
+        break
+      case '÷':
+        computation = prev / current 
+        break
+      default:
+        return
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+  }
+
+  getDisplayNumber(number) {
+    return number
+  }
+  //updates display of calculator
+  updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+    if (this.operation != null) {
+      this.previousOperandTextElement.innerText = 
+      `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+    }
+    this.previousOperandTextElement.innerText = this.previousOperand
+    } else { 
+    this.previousOperandTextElement.innerText = ''
+    }
+  }
+}
+
 //operator functions
 
 function add(a , b) {
@@ -16,57 +94,46 @@ function divide(a, b) {
   return a / b;
 }
 
-// function with switch case that takes operator and two numbers then calls on operator function.
-
-function operate(num1, num2, operator) {
-  switch (operator) {
-    case "+":
-      return add(num1, num2);
-    case "-":
-      return subtract(num1, num2);
-    case "*":
-      return multiply(num1, num2);
-    case "/":
-      return divide(num1, num2);  
-  }
-};
 
 //DOM elements for inputing numbers
 
-const numberButton = document.querySelectorAll ('.number');
-const operatorButton = document.querySelectorAll('.operator');
-const clearbutton = document.querySelector('.clear');
-const deleteButton = document.querySelector('.delete');
-const equalsButton = document.querySelector('.result');
-const currentOperand = document.querySelector('.current');
-const lastOperand = document.querySelector('.last');
+const numberButtons = document.querySelectorAll ('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-currentOperand.textContent = ' ';
-lastOperand.textContent = ' ';
 
-//functions that populate the display of calculator and store the display value
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
 
-let storedNumber = '';
-let clickedOperator = '';
-let firstNumber = '';
-let result = '';
-currentOperand.textContent = 0;
-
-numberButton.forEach((number) => {
-  number.addEventListener('click' , function(){
-    storedNumber += number.ariaValueMax;
-    currentOperand.textContent = storedNumber;
+// Functions for displaying number buttons and operation buttons to calculator on click
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
   })
-});
+})
 
-operatorButton.forEach((operator => {
-  operator.addEventListener('click', function() {
-    firstNumber = storedNumber;
-
-    clickedOperator = operator.textContent;
-    lastOperand.textContent = storedNumber + clickedOperator;
-
+operationButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.chooseOperation(button.innerText)
+    calculator.updateDisplay()
   })
-}));
+})
 
+equalsButton.addEventListener('click', button => {
+  calculator.compute()
+  calculator.updateDisplay()
+})
 
+clearButton.addEventListener('click', button => {
+  calculator.clear()
+  calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+  calculator.delete()
+  calculator.updateDisplay()
+})
